21f0722993772b26d7b59d4e72ff6a079ef25083
dami <midoks@163.com>
2026-05-15 01:38:00 +0800

    Update tgbot_signad.go

--- internal/model/tgbot_signad.go
+++ internal/model/tgbot_signad.go
@@ -1,10 +1,10 @@
 package model
 
 type TgbotSignAd struct {
-	ID           int64  `json:"id" gorm:"primaryKey"` // unique key
-	UserID       int64  `json:"user_id"`              // user id
-	FromUserName string `json:"from_user_name"`       // from user name
-	Status       bool   `json:"status"`               // status
-	CreateTime   int64  `json:"create_time"`          // create time
-	UpdateTime   int64  `json:"update_time"`          // update_time
+	ID           int64  `json:"id" gorm:"primaryKey"`       // unique key
+	UserID       int64  `json:"user_id" gorm:"uniqueIndex"` // user id
+	FromUserName string `json:"from_user_name"`             // from user name
+	Status       bool   `json:"status"`                     // status
+	CreateTime   int64  `json:"create_time"`                // create time
+	UpdateTime   int64  `json:"update_time"`                // update_time
 }
